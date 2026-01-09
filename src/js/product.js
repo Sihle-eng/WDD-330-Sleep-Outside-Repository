9878638813bf3e110e976981f8b69e77193db0c2
budujoseph <budu.joseph95@gmail.com>
2026-01-09 13:07:14 +0000

    fixed broken LS

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,20 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cart = getLocalStorage("so-cart") || [];
+  // check if product is already in cart
+  const productInCart = cart.find((item) => item.id === product.id);
+  if (productInCart) {
+    alert("This item is already in your cart.");
+    return;
+  }
+
+  cart.push(product); // add product to cart array
+  setLocalStorage("so-cart", cart); // save updated cart to local storage
+  alert("Product added to cart!");
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
